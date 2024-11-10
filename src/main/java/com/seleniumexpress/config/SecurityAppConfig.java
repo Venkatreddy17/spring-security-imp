3e1c4e89825bf44665d91a79b6320a0b4767224a
Shivannagarivenkat1706@gmail.com <shivannagarivenkat1706@gmail.com>
2024-11-10 12:49:22 +0530

    add user name and password for login

--- src/main/java/com/seleniumexpress/config/SecurityAppConfig.java
+++ src/main/java/com/seleniumexpress/config/SecurityAppConfig.java
@@ -1,10 +1,40 @@
 package com.seleniumexpress.config;
 
+import java.util.ArrayList;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity(debug = true)
 public class SecurityAppConfig {
 
+	@Bean
+	public InMemoryUserDetailsManager setUpUser(){
+		ArrayList<GrantedAuthority> authoritiesList=new ArrayList<>();
+		authoritiesList.add(new SimpleGrantedAuthority("admin"));
+		authoritiesList.add(new SimpleGrantedAuthority("user"));
+		
+		UserDetails abhilashUser=new User("venkat","venkat",authoritiesList);
+		
+		InMemoryUserDetailsManager inMemoryUserDetailsManager=new InMemoryUserDetailsManager();
+		inMemoryUserDetailsManager.createUser(abhilashUser);
+		
+		return inMemoryUserDetailsManager;
+	}
+	
+	@Bean
+	 PasswordEncoder passwordEncoder(){
+		
+		 return NoOpPasswordEncoder.getInstance();
+	}
+	
 }
